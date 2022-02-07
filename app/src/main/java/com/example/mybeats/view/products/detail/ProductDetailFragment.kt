b5f2ba4ecf8398d27de736e8846f7207cc1d427c
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-07 11:55:19 -0300

    Created the fragment detail layout and update the nav_graph to send a product like argument to fragment

--- app/src/main/java/com/example/mybeats/view/products/detail/ProductDetailFragment.kt
+++ app/src/main/java/com/example/mybeats/view/products/detail/ProductDetailFragment.kt
@@ -1,15 +1,18 @@
 package com.example.mybeats.view.products.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.mybeats.databinding.FragmentProductDetailBinding
 
 class ProductDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentProductDetailBinding
+    private val args: ProductDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +20,24 @@ class ProductDetailFragment : Fragment() {
     ): View {
         binding = FragmentProductDetailBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+        val product = args.product
+        binding.apply {
+            tvModel.text = product.model
+            tvConnection.text = product.connection
+            tvCompatibility.text = product.compatibility
+            tvPowerSupply.text = product.powerSupply
+            tvAutonomy.text = product.autonomy
+            tvHeight.text = product.height
+            tvCapture.text = product.capture
+        }
+
     }
 }
