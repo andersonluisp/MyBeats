45e032aeab4302ff7a839744ce4316d286699f05
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-08 18:26:32 -0300

    Setup fullscreen activity

--- app/src/main/java/com/example/mybeats/view/login/MainActivity.kt
+++ app/src/main/java/com/example/mybeats/view/login/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.mybeats.view.login
 
+import android.annotation.TargetApi
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import com.example.mybeats.databinding.ActivityMainBinding
 import com.example.mybeats.view.products.ProductsActivity
 
@@ -14,7 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupFullScreen()
         setupListeners()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setupFullScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 
     private fun setupListeners(){
