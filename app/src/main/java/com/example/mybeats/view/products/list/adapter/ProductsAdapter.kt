4bb41380d7a9e0abdeaa4ae800d4dcb802824910
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-09 10:47:27 -0300

    Added filterList method to ProductsAdapter.kt and exported strings to values

--- app/src/main/java/com/example/mybeats/view/products/list/adapter/ProductsAdapter.kt
+++ app/src/main/java/com/example/mybeats/view/products/list/adapter/ProductsAdapter.kt
@@ -6,10 +6,14 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.mybeats.R
 import com.example.mybeats.data.model.Product
 import com.example.mybeats.databinding.ProductItemListBinding
+import java.util.*
 
 class ProductsAdapter(val onItemClick: (Product) -> Unit): ListAdapter<Product, ProductsAdapter.ProductsAdapterViewHolder>(ProductsDiffUtil()) {
+
+    private var productsList = mutableListOf<Product>()
 
     inner class ProductsAdapterViewHolder(
         private val binding: ProductItemListBinding
@@ -17,8 +21,8 @@ class ProductsAdapter(val onItemClick: (Product) -> Unit): ListAdapter<Product, 
         fun bind(item: Product){
             binding.tvProductName.text = item.model
             binding.tvRating.text = item.rating.toString()
-            binding.tvProductPrice.text = "R$ " + item.price
-            binding.tvReviews.text = item.reviews.toString() + " Reviews"
+            binding.tvProductPrice.text = binding.root.context.resources.getString(R.string.BRL, item.price)
+            binding.tvReviews.text = binding.root.context.resources.getQuantityString(R.plurals.reviews_count, item.reviews, item.reviews)
 
             Glide.with(binding.root)
                 .load(item.image)
@@ -43,6 +47,23 @@ class ProductsAdapter(val onItemClick: (Product) -> Unit): ListAdapter<Product, 
     override fun onBindViewHolder(holderAdapter: ProductsAdapterViewHolder, position: Int) {
         holderAdapter.bind(getItem(position))
     }
+
+    fun filterList(filterText: String?){
+        val filteredList = mutableListOf<Product>()
+        if (filterText.isNullOrEmpty()){
+            filteredList.addAll(productsList)
+        } else {
+            val result = productsList.filter { it.model.lowercase(Locale.getDefault()).contains(filterText.lowercase(Locale.getDefault())) }
+            filteredList.addAll(result)
+        }
+        submitList(filteredList)
+    }
+
+    fun setData(list: List<Product>){
+        productsList = list.toMutableList()
+        submitList(list)
+    }
+
 }
 
 class ProductsDiffUtil: DiffUtil.ItemCallback<Product>() {
