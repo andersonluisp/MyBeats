65117d1095008c90b6cb52c01c5a3a295a60486b
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-09 10:53:00 -0300

    Added searchView to ProductsListFragment.kt

--- app/src/main/java/com/example/mybeats/view/products/list/ProductsListFragment.kt
+++ app/src/main/java/com/example/mybeats/view/products/list/ProductsListFragment.kt
@@ -1,14 +1,14 @@
 package com.example.mybeats.view.products.list
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mybeats.R
 import com.example.mybeats.databinding.FragmentProductsListBinding
 import com.example.mybeats.view.products.list.adapter.ProductsAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -17,10 +17,13 @@ class ProductsListFragment : Fragment() {
 
     private lateinit var binding: FragmentProductsListBinding
     private val viewModel: ProductsViewModel by viewModel()
-    private val productsAdapter: ProductsAdapter by lazy { ProductsAdapter(){
-        val direction = ProductsListFragmentDirections.actionProductsListFragmentToProductDetailFragment(it)
-        binding.root.findNavController().navigate(direction)
-    } }
+    private val productsAdapter: ProductsAdapter by lazy {
+        ProductsAdapter() {
+            val direction =
+                ProductsListFragmentDirections.actionProductsListFragmentToProductDetailFragment(it)
+            binding.root.findNavController().navigate(direction)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,9 +36,27 @@ class ProductsListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupSearchView()
         setupObservers()
         setupRecyclerView()
         viewModel.getProducts()
+    }
+
+    private fun setupSearchView() {
+        binding.toolbar.inflateMenu(R.menu.search_menu)
+        val menuItem = binding.toolbar.menu.findItem(R.id.action_search)
+        val searchView = menuItem.actionView as SearchView
+        searchView.queryHint = requireActivity().getString(R.string.label_search)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                productsAdapter.filterList(newText)
+                return true
+            }
+        })
     }
 
     private fun setupRecyclerView() {
@@ -47,8 +68,8 @@ class ProductsListFragment : Fragment() {
     }
 
     private fun setupObservers() {
-        viewModel.products.observe(requireActivity()){
-            productsAdapter.submitList(it)
+        viewModel.products.observe(requireActivity()) {
+            productsAdapter.setData(it)
         }
     }
 }
