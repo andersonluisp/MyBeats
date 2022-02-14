b445b3acf203e2551702f1823876a231bc71602f
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-14 18:08:48 -0300

    Setup the navcontroller

--- app/src/main/java/com/example/mybeats/view/products/ProductsActivity.kt
+++ app/src/main/java/com/example/mybeats/view/products/ProductsActivity.kt
@@ -2,15 +2,33 @@ package com.example.mybeats.view.products
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.mybeats.R
 import com.example.mybeats.databinding.ActivityProductsBinding
 
 class ProductsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductsBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavController()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavController(){
+        val homeContainerView = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = homeContainerView.navController
+        setupActionBarWithNavController(navController)
     }
 }
