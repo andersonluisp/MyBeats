58cdbc038f2e8668501277d42f6afb5acb6636cf
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-14 18:08:32 -0300

    Setup the supportActionBar

--- app/src/main/java/com/example/mybeats/view/products/detail/ProductDetailFragment.kt
+++ app/src/main/java/com/example/mybeats/view/products/detail/ProductDetailFragment.kt
@@ -1,11 +1,13 @@
 package com.example.mybeats.view.products.detail
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.mybeats.R
 import com.example.mybeats.databinding.FragmentProductDetailBinding
@@ -25,14 +27,16 @@ class ProductDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupNavigationIcon()
+        setupActionBar()
         setupView()
     }
 
-    private fun setupNavigationIcon(){
-        binding.toolbar.setNavigationIcon(R.drawable.ic_back)
-        binding.toolbar.setNavigationOnClickListener {
-            requireActivity().findNavController(R.id.nav_host_fragment_container).popBackStack()
+    private fun setupActionBar() {
+        (activity as AppCompatActivity).supportActionBar?.apply {
+            elevation = 0f
+            setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(requireContext(), R.color.toolbar_color)))
+            title = ""
+            setHomeAsUpIndicator(R.drawable.ic_back)
         }
     }
 
