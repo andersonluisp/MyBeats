1aab410ff0d3b3353a6df8701e81241572a52953
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-17 15:17:28 -0300

    Commit to test the action workflow when PR is open to main

--- app/src/main/java/com/example/mybeats/view/login/MainActivity.kt
+++ app/src/main/java/com/example/mybeats/view/login/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setupFullScreen()
         setupListeners()
+        val teste = "teste"
     }
 
     @Suppress("DEPRECATION")
