ebd7d9666665eafcf5663e6c09505b596e9ee83c
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-23 18:23:15 -0300

    Update the ProductsApi to provide a Response class

--- app/src/main/java/com/example/mybeats/data/remote/api/ProductsApi.kt
+++ app/src/main/java/com/example/mybeats/data/remote/api/ProductsApi.kt
@@ -1,9 +1,10 @@
 package com.example.mybeats.data.remote.api
 
 import com.example.mybeats.data.remote.model.ProductsBody
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ProductsApi {
     @GET("eab81402-d76f-4433-96bf-39541646744e")
-    suspend fun getProducts(): ProductsBody
+    suspend fun getProducts(): Response<ProductsBody>
 }
