f662d331aa616b908d76ba7738c45de59370a215
Anderson Pimentel <andersonluiscp@gmail.com>
2022-02-24 14:27:26 -0300

    Transform the Response.toErrorResponse into a generic extension function

--- app/src/main/java/com/example/mybeats/data/remote/extension/ResultRemoteExtension.kt
+++ app/src/main/java/com/example/mybeats/data/remote/extension/ResultRemoteExtension.kt
@@ -1,12 +1,11 @@
 package com.example.mybeats.data.remote.extension
 
-import com.example.mybeats.data.remote.model.ProductsBody
 import com.example.mybeats.data.remote.responses.ResultRemote
 import com.example.mybeats.data.remote.responses.ResultRemote.NetworkErrors
 import retrofit2.Response
 
-fun Response<ProductsBody>.toErrorResponse(): ResultRemote.ErrorResponse {
-    val networkError = NetworkErrors.values().firstOrNull() {
+fun <T> Response<T>.toErrorResponse(): ResultRemote.ErrorResponse {
+    val networkError = NetworkErrors.values().firstOrNull {
         it.code == this.code()
     }
     return if (networkError != null) {
